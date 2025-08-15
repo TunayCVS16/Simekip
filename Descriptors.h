2fc650cdca2ccdd2b1e801064c6547af812d8fba
TunayCVS16 <tunay84cavusoglu@gmail.com>
2025-08-15 08:05:18 +0300

    Update Descriptors.h

--- Descriptors.h
+++ Descriptors.h
@@ -1,10 +1,13 @@
 #pragma once
 #include <avr/pgmspace.h>
 #include <LUFA/Drivers/USB/USB.h>
-#include <LUFA/Drivers/USB/Class/Common/HIDReportData.h>   // HID_RI_* makroları
+#include <LUFA/Drivers/USB/Class/Common/HIDReportData.h>
 
+// Joystick report array tanımı (Descriptors.c içinde tanımlı)
 extern const USB_Descriptor_HIDReport_Datatype_t PROGMEM JoystickReport[];
 
-uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue, const uint8_t wIndex,
+// LUFA'nın beklediği imza (sadece prototype)
+uint16_t CALLBACK_USB_GetDescriptor(const uint16_t wValue,
+                                    const uint8_t wIndex,
                                     const void** const DescriptorAddress)
-  ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
+                                    ATTR_WARN_UNUSED_RESULT ATTR_NON_NULL_PTR_ARG(3);
